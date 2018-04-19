f5e358642431da760c52f737c72f00889f4c2ed0
DDPerez08 <ddperez08@gmail.com>
2018-04-19 00:11:03 -0300

    tp
    archivos para base

---
+++ src/main/java/user.java
@@ -0,0 +1,41 @@
+public class user {
+
+    private Integer id;
+    private String Nombre;
+    private String Clave;
+    private String Estado;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getClave() {
+        return Clave;
+    }
+
+    public void setClave(String clave) {
+        Clave = clave;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String estado) {
+        Estado = estado;
+    }
+
+    
+}
